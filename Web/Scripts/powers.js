2aff8e5965d9519b05878816d590d4c16de2d4e6
MouseOfLight <MouseOfLight@kfee-programming.com>
2017-09-17 13:48:45 -0700

    Moved powers names into the object

--- Web/Scripts/powers.js
+++ Web/Scripts/powers.js
@@ -1,43 +1,52 @@
-﻿
-window.powers = {
-    "support": {
-        neighbor: {
-            "all": true,
-            "bonus": 1,
-            "phase": "defense",
-        },
-        "receivesBonus": true,
-    },
-    "squire": {
-        "squire": true,
-    },
-    "stonewall": {
-        "neighbor":
+﻿(function() {
+    var powers = [
         {
-            "dice": 1,
-            "phase": "defense",
-            "self": true,
+            name: "support",
+            neighbor: {
+                "all": true,
+                "bonus": 1,
+                "phase": "defense",
+            },
+            "receivesBonus": true,
         },
-    },
-    "speedy": {
-        "action": 1,
-        "afterAttack": "move",
-    },
-    "melee": {
-        "neighbor": {
-            "bonus": 3,
-            "neighborOnly": true,
-            "self": true,
+        {
+            name: "squire",
+            "squire": true,
         },
-    },
-    "commander": {
-        "bonusAction": 1,
-    },
-    "officer": {
-        "maxSquireIncrement": 2
-    },
-    "artillery": {
-        "unblocked": true,
-        "afterAttack": "attack",
-    },
-};
+        {
+            name: "stonewall",
+            "neighbor":
+            {
+                "dice": 1,
+                "phase": "defense",
+                "self": true,
+            },
+        },
+        {
+            name: "speedy",
+            "action": 1,
+            "afterAttack": "move",
+        },
+        {
+            name: "melee",
+            "neighbor": {
+                "bonus": 3,
+                "neighborOnly": true,
+                "self": true,
+            },
+        },
+        {
+            name: "commander",
+            "bonusAction": 1,
+        },
+        {
+            name: "officer",
+            "maxSquireIncrement": 2
+        },
+        {
+            name: "artillery",
+            "unblocked": true,
+            "afterAttack": "attack",
+        },
+    ];
+});
