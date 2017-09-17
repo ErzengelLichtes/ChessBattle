e0826f717bbad769205da1f037494b578250ce5c
MouseOfLight <MouseOfLight@kfee-programming.com>
2017-09-17 08:50:45 -0700

    Moved powers into its own file

--- Web/Scripts/board.js
+++ Web/Scripts/board.js
@@ -41,48 +41,6 @@ function Piece(settings) {
     this.maximum = settings.maximum || 1;
 }
 
-window.powers = {
-    "support": {
-        neighbor: {
-            "all": true,
-            "bonus": 1,
-            "phase": "defense",
-        },
-        "receivesBonus": true,
-    },
-    "squire": {
-        "squire": true,
-    },
-    "stonewall": {
-        "neighbor":
-        {
-            "dice": 1,
-            "phase": "defense",
-            "self": true,
-        },
-    },
-    "speedy": {
-        "action": 1,
-        "afterAttack": "move",
-    },
-    "melee": {
-        "neighbor": {
-            "bonus": 3,
-            "neighborOnly": true,
-            "self": true,
-        },
-    },
-    "commander": {
-        "bonusAction": 1,
-    },
-    "officer": {
-        "maxSquireIncrement": 2
-    },
-    "artillery": {
-        "unblocked":true,
-        "afterAttack":"attack",
-    },
-};
 
 $(document).ready(function () {
     var testBoard = new Board(8, 8);
---
+++ Web/Scripts/powers.js
@@ -0,0 +1,43 @@
+﻿
+window.powers = {
+    "support": {
+        neighbor: {
+            "all": true,
+            "bonus": 1,
+            "phase": "defense",
+        },
+        "receivesBonus": true,
+    },
+    "squire": {
+        "squire": true,
+    },
+    "stonewall": {
+        "neighbor":
+        {
+            "dice": 1,
+            "phase": "defense",
+            "self": true,
+        },
+    },
+    "speedy": {
+        "action": 1,
+        "afterAttack": "move",
+    },
+    "melee": {
+        "neighbor": {
+            "bonus": 3,
+            "neighborOnly": true,
+            "self": true,
+        },
+    },
+    "commander": {
+        "bonusAction": 1,
+    },
+    "officer": {
+        "maxSquireIncrement": 2
+    },
+    "artillery": {
+        "unblocked": true,
+        "afterAttack": "attack",
+    },
+};
